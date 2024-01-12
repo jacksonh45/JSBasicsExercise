aaeee352297d51dcf9837b5c5e3a152b37faa8e7
Jackson Harrison <Jackson.Harrison@stu.barren./kyschools.us>
2024-01-12 08:03:07 -0600

    Completed both assignment and bonus

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log(11);
+//Numbers, Strings, Booleans, Null, Undefined
+// +, -, *, \, **, %
+console.log(5 % 2);
+console.log(5 % 4);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Hawk" + "Eye");
+console.log("Jackson");
+console.log("Harrison");
+console.log("Jackson".length + "Harrison".length);
+console.log("Avengers: Endgame"[16]);
